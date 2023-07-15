88f75d66dc60fcc2538037296f51e9242163a39c
Mikazuky93 <ahmedbadawy.ph@gmail.com>
2023-07-15 03:25:11 +0300

    0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x = 'a';
+	char X = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+
+	while (X <= 'Z')
+	{
+		putchar(X);
+		X++;
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
